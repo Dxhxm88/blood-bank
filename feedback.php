c0d3fa44f44b915669567f9b4442040fbedd8bcd
Dxhxm88 <ahmadaham99@gmail.com>
2022-03-12 13:01:31 +0800

    Add feedback function

--- feedback.php
+++ feedback.php
@@ -8,17 +8,17 @@
     <div class="media">
         <img src="https://cdn.pixabay.com/photo/2018/04/12/04/26/blood-pressure-3312513_960_720.png" class="mr-3 w-50" alt="...">
         <div class="media-body">
-            <form>
+            <form action="https://formspree.io/f/xeqnkydw" method="POST">
                 <h2 class="w3-center">Feedback</h2>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" require>
+                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" require>
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1">Feedback</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" require></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message" require></textarea>
                 </div>
-                <button type="submit" onclick="alert('Feedback has delivered')" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
